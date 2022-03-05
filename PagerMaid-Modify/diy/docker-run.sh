4682084571110c50fc6bff17b51b96cbcb1cd713
kangwenhang <kangwenhang@hotmail.com>
2022-03-05 20:35:56 +0800

    update

--- PagerMaid-Modify/diy/docker-run.sh
+++ PagerMaid-Modify/diy/docker-run.sh
@@ -62,11 +62,11 @@ configure() {
           sed -i "s/503691334/$log_chatid/" $config_file
         fi
         sed -i "s/log: False/log: True/" $config_file
-        cp -rf $config_fil /pagermaid/workdir/config.yml
+        cp -rf $config_file /pagermaid/workdir/config.yml
         ;;
       [nN][oO]|[nN])
         echo "安装过程继续 . . ."
-        cp -rf $config_fil /pagermaid/workdir/config.yml
+        cp -rf $config_file /pagermaid/workdir/config.yml
         ;;
       *)
         echo "输入错误 . . ."
