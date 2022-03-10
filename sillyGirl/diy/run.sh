b94e974b05390c62c1663762a92c7dea301852d8
kangwenhang <kangwenhang@hotmail.com>
2022-03-11 02:21:59 +0800

    update

--- sillyGirl/diy/run.sh
+++ sillyGirl/diy/run.sh
@@ -5,11 +5,15 @@ if [ -f "/etc/sillyGirl/sets.conf" -a -f "/etc/sillyGirl/sillyGirl.cache" ];then
   pm2 start ./sillyGirl
   pm2 log
 else 
-  echo "未检测到配置文件，首次运行傻妞"
+  echo "未检测到配置文件，请先自行配置相关设置参数"
+  7za x /sillyGirl/admin.zip -r -o/sillyGirl/plugin/web/admin/
   cp -rf /sillyGirl/sets.conf.sample /etc/sillyGirl/sets.conf
   sleep 2
   ./sillyGirl -t
-  echo "运行完成，请重启容器"
+  echo "运行完成，请进入数据面板配置相关参数，地址：Http://傻妞地址/admin"
+  echo "初始用户名：admin"
+  echo "初始密码：admin"
+  echo "完成后请重启容器，命令：docker restart 容器名称"
 fi
 
 tail -f /dev/null
