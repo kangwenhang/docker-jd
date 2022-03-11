39895dff53a2483baa268f5e6f3e4e31c4e096dc
kangwenhang <kangwenhang@hotmail.com>
2022-03-12 02:47:14 +0800

    Update run.sh

--- sillyGirl/diy/run.sh
+++ sillyGirl/diy/run.sh
@@ -8,12 +8,12 @@ else
   echo "未检测到配置文件，请先自行配置相关设置参数"
   7za x /sillyGirl/admin.zip -r -o/sillyGirl/plugin/web/admin/
   cp -rf /sillyGirl/sets.conf.sample /etc/sillyGirl/sets.conf
-  sleep 2
-  ./sillyGirl -t
   echo "运行完成，请进入数据面板配置相关参数，地址：Http://傻妞地址/admin"
   echo "初始用户名：admin"
   echo "初始密码：admin"
   echo "完成后请重启容器，命令：docker restart 容器名称"
+  sleep 5
+  ./sillyGirl -t
 fi
 
 tail -f /dev/null
