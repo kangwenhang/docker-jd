8f1cefe33b4a82c0e8f95fe70ed7c6294f468689
kangwenhang <kangwenhang@hotmail.com>
2022-03-12 03:18:02 +0800

    update

--- sillyGirl/diy/run.sh
+++ sillyGirl/diy/run.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-if [ -f "/etc/sillyGirl/sets.conf" -a -f "/etc/sillyGirl/sillyGirl.cache" ];then
-  echo "检测到配置，开始运行傻妞"
-  pm2 start ./sillyGirl
-  pm2 log
-else 
-  echo "未检测到配置文件，请先自行配置相关设置参数"
-  7za x /sillyGirl/admin.zip -r -o/sillyGirl/plugin/web/admin/
+too() {
   cp -rf /sillyGirl/sets.conf.sample /etc/sillyGirl/sets.conf
-  echo "运行完成，请进入数据面板配置相关参数，地址：Http://傻妞地址/admin"
+  echo "运行完成后，请进入数据面板配置相关参数，地址：Http://傻妞地址/admin"
   echo "初始用户名：admin"
   echo "初始密码：admin"
   echo "完成后请重启容器，命令：docker restart 容器名称"
   sleep 5
   ./sillyGirl -t
+}
+
+if [ -f "/etc/sillyGirl/sets.conf" -a -f "/etc/sillyGirl/sillyGirl.cache" ];then
+  echo "检测到配置，开始运行傻妞"
+  pm2 start ./sillyGirl
+  pm2 log
+else 
+  echo "未检测到配置文件，请先自行配置相关设置参数，开始首次运行傻妞"
+  if [ "`ls -A /sillyGirl/plugin/web/admin/`" = "" ]; then
+    echo "数据web管理插件存在，跳过安装"
+    too
+  else
+    echo "数据web管理插件不存在，开始安装"
+    7za x /sillyGirl/admin.zip -r -o/sillyGirl/plugin/web/admin/
+    echo "数据web管理插件安装成功"
+    too
+  fi
 fi
 
 tail -f /dev/null
