85244aadc990196a66779945cbd7eccced1bf84c
kangwenhang <kangwenhang@hotmail.com>
2022-03-12 04:11:57 +0800

    update

--- sillyGirl/diy/run.sh
+++ sillyGirl/diy/run.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
+too1() {
+  echo "运行完成后，请进入数据面板配置相关参数，地址：Http://傻妞地址/admin"
+  echo "初始用户名：admin"
+  echo "初始密码：admin"
+  echo "完成后请重启容器，命令：docker restart 容器名称"
+  sleep 5
+  ./sillyGirl -t
+}
 
 too() {
-  if [ -e "/etc/sillyGirl/sets.conf" ];then
-    echo "设置文件存在，进行下一步"
-    echo "运行完成后，请进入数据面板配置相关参数，地址：Http://傻妞地址/admin"
-    echo "初始用户名：admin"
-    echo "初始密码：admin"
-    echo "完成后请重启容器，命令：docker restart 容器名称"
-    sleep 5
-    ./sillyGirl -t
-  else
-    echo "设置文件不存在，开始使用默认设置文件"
+  if [ ! -d "/etc/sillyGirl/" ];then
+    echo "设置文件夹不存在，创建文件夹并使用默认配置"
+    mkdir -p /etc/sillyGirl/
     cp -rf /sillyGirl/sets.conf.sample /etc/sillyGirl/sets.conf
-    echo "运行完成后，请进入数据面板配置相关参数，地址：Http://傻妞地址/admin"
-    echo "初始用户名：admin"
-    echo "初始密码：admin"
-    echo "完成后请重启容器，命令：docker restart 容器名称"
-    sleep 5
-    ./sillyGirl -t
+    too1
+  else
+    echo "设置文件夹存在，进行下一步"
+    if [ -e "/etc/sillyGirl/sets.conf" ];then
+      echo "设置文件存在，进行下一步"
+      too1
+    else
+      echo "设置文件不存在，开始使用默认设置文件"
+      cp -rf /sillyGirl/sets.conf.sample /etc/sillyGirl/sets.conf
+      too1
+    fi
   fi
+
+
 }
 
 if [ -f "/etc/sillyGirl/sets.conf" -a -f "/etc/sillyGirl/sillyGirl.cache" ];then
@@ -34,7 +42,7 @@ else
     too
   else
     if [ "`ls -A /sillyGirl/plugin/web/admin/`" = "" ]; then
-      echo "数据web管理插件存在，但为空"
+      echo "数据web管理插件文件夹存在，但内容为空"
       7za x /sillyGirl/admin.zip -r -o/sillyGirl/plugin/web/admin/
       echo "数据web管理插件安装成功"
       too
