12ed3a32197df9d03445853704b3850e0c062af6
kangwenhang <kangwenhang@hotmail.com>
2022-03-16 02:18:02 +0800

    update

--- sillyGirl/diy/run.sh
+++ sillyGirl/diy/run.sh
@@ -1,31 +1,12 @@
 #!/bin/bash
 
-too1() {
+too() {
   echo "运行完成后，请进入数据面板配置相关参数，地址：Http://傻妞地址/admin"
   echo "初始用户名：admin"
-  echo "初始密码：admin  ，请去config文件夹下修改密码（变量名为set sillyGirl adminPassword）"
+  echo "初始密码：随机，请查看日志"
   echo "完成后请重启容器，命令：docker restart 容器名称"
   sleep 5
   ./sillyGirl -t
-}
-
-too() {
-  if [ ! -d "/etc/sillyGirl/" ];then
-    echo "设置文件夹不存在，创建文件夹并使用默认配置"
-    mkdir -p /etc/sillyGirl/
-    cp -rf /sillyGirl/sets.conf.sample /etc/sillyGirl/sets.conf
-    too1
-  else
-    echo "设置文件夹存在，进行下一步"
-    if [ -e "/etc/sillyGirl/sets.conf" ];then
-      echo "设置文件存在，进行下一步"
-      too1
-    else
-      echo "设置文件不存在，开始使用默认设置文件"
-      cp -rf /sillyGirl/sets.conf.sample /etc/sillyGirl/sets.conf
-      too1
-    fi
-  fi
 }
 
 if [ -f "/etc/sillyGirl/sets.conf" -a -f "/etc/sillyGirl/sillyGirl.cache" ];then
