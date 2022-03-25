607872334f24b947c35088f123f47f7bb4ee8048
kangwenhang <kangwenhang@hotmail.com>
2022-03-26 01:51:41 +0800

    update

---
+++ sillyGirl/diy/docker-run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+too() {
+  echo "运行完成后，请进入数据面板配置相关参数，地址：Http://傻妞地址/admin"
+  echo "初始用户名：admin"
+  echo "初始密码：随机，请查看日志"
+  echo "完成后请重启容器，命令：docker restart 容器名称"
+  sleep 5
+  cd /sillyGirl
+  ./sillyGirl -t
+}
+
+oicq() {
+  if [ $oicqbot = "true" ]; then
+    if [ -f "/root/.oicq/config.js" ]; then
+      echo "检测到配置文件，首次启动oicq进行确认"
+      echo "自动匹配到如下QQ号"
+      grep -oP '[1-9][0-9]{4,10}:' /root/.oicq/config.js 2>&1 | tee /sillyGirl/qq.log >/dev/null 2>&1
+      if [[ `cat /sillyGirl/qq.log |wc -l` -eq 0 ]]; then
+        echo "错误！未检测到QQ号，请确认配置是否已填写"
+        return
+      else
+        sed -i 's/://g' /sillyGirl/qq.log
+        cat /sillyGirl/qq.log
+        botqq=$(cat /sillyGirl/qq.log)
+        echo "开始启动oicq"
+        cd /root/.oicq/
+        oicq $botqq
+        fi
+      fi
+    else
+      echo "未检测到配置文件，开始生成默认配置文件"
+      cp -rf /sillyGirl/config.js.sample /root/.oicq/config.js
+      echo "生成完成，请自行修改配置文件"
+    fi
+  else
+    echo "已选择不开启oicq"
+  fi
+}
+
+oicq
+if [ -d "/sillyGirl/plugin/web/admin/" ] && [ ! "`ls -A /sillyGirl/plugin/web/admin/`" = "" ]; then
+  echo "数据web管理插件存在，跳过安装"
+  too
+else
+  echo "数据web管理插件不存在，开始安装"
+  7za x /sillyGirl/admin.zip -r -o/sillyGirl/plugin/web/admin/
+  echo "数据web管理插件安装成功"
+  too
+fi
+
+tail -f /dev/null
+
+exec "$@"
--- sillyGirl/diy/run.sh
+++ sillyGirl/diy/run.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-too() {
-  echo "运行完成后，请进入数据面板配置相关参数，地址：Http://傻妞地址/admin"
-  echo "初始用户名：admin"
-  echo "初始密码：随机，请查看日志"
-  echo "完成后请重启容器，命令：docker restart 容器名称"
-  sleep 5
-  cd /sillyGirl
-  ./sillyGirl -t
-}
-
 oicq() {
   if [ $oicqbot = "true" ]; then
     if [ -f "/root/.oicq/config.js" ]; then
       echo "检测到配置文件，开始启动qqbot"
       echo "自动匹配到如下QQ号"
       grep -oP '[1-9][0-9]{4,10}:' /root/.oicq/config.js 2>&1 | tee /sillyGirl/qq.log >/dev/null 2>&1
-      sed -i 's/://g' /sillyGirl/qq.log
-      cat /sillyGirl/qq.log
-      echo "开始启动oicq"
-      $botqq=$(cat /sillyGirl/qq.log)
-      cd /root/.oicq/
-      pm2 start "oicq $botqq"
+      if [[ `cat /sillyGirl/qq.log |wc -l` -eq 0 ]]; then
+        echo "错误！未检测到QQ号，请确认配置是否已填写"
+        return
+      else
+        sed -i 's/://g' /sillyGirl/qq.log
+        cat /sillyGirl/qq.log
+        botqq=$(cat /sillyGirl/qq.log)
+        if [ -d "/root/.oicq/$botqq" ]; then
+          echo "开始启动oicq"
+          cd /root/.oicq/
+          pm2 start "oicq $botqq"
+        else
+          echo "登录信息不存在，请检查登录信息"
+          return
+        fi
+      fi
     else
-      echo "开始生成默认配置文件"
-      cp -rf /sillyGirl/config.js.sample /root/.oicq/config.js
-      echo "生成完成，请修改配置文件后重启容器。重启命令：docker restart 容器名称"
+      echo "未检测到配置文件，请使用命令："
+      echo "容器外：docker exec -it 容器名称 bash /sillyGirl/docker-run.sh"
+      echo "容器内：bash /sillyGirl/docker-run.sh"
     fi
   else
     echo "已选择不开启oicq"
@@ -34,28 +34,23 @@ oicq() {
 
 oicq
 if [ -f "/etc/sillyGirl/sillyGirl.cache" ]; then
-  echo "检测到配置，开始运行傻妞"
-  cd /sillyGirl
-  pm2 start ./sillyGirl
-  pm2 log
-else 
-  echo "未检测到配置文件，请先自行配置相关设置参数，开始首次运行傻妞"
-  if [ ! -d "/sillyGirl/plugin/web/admin/" ]; then
-    echo "数据web管理插件不存在，开始安装"
-    7za x /sillyGirl/admin.zip -r -o/sillyGirl/plugin/web/admin/
-    echo "数据web管理插件安装成功"
-    too
+  if [ -d "/sillyGirl/plugin/web/admin/" ] && [ ! "`ls -A /sillyGirl/plugin/web/admin/`" = "" ]; then
+    echo "检测到配置，开始运行傻妞"
+    cd /sillyGirl
+    pm2 start ./sillyGirl
+    pm2 log
   else
-    if [ "`ls -A /sillyGirl/plugin/web/admin/`" = "" ]; then
-      echo "数据web管理插件文件夹存在，但内容为空"
-      7za x /sillyGirl/admin.zip -r -o/sillyGirl/plugin/web/admin/
-      echo "数据web管理插件安装成功"
-      too
-    else
-      echo "数据web管理插件存在，跳过安装"
-      too
-    fi
+    echo "面板异常，开始恢复面板"
+    7za x /sillyGirl/admin.zip -r -o/sillyGirl/plugin/web/admin/
+    echo "面板恢复成功，开始启动傻妞"
+    cd /sillyGirl
+    pm2 start ./sillyGirl
+    pm2 log
   fi
+else
+  echo "未检测到配置文件，请使用命令："
+  echo "容器外：docker exec -it 容器名称 bash /sillyGirl/docker-run.sh"
+  echo "容器内：bash /sillyGirl/docker-run.sh"
 fi
 
 tail -f /dev/null
