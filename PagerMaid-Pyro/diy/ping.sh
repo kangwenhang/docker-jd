ec15e7aada0ffaad9bff5003db43a69f3e4f38c1
kangwenhang <kangwenhang@hotmail.com>
2023-02-22 17:34:57 +0000

    update

--- PagerMaid-Pyro/diy/ping.sh
+++ PagerMaid-Pyro/diy/ping.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #自定义变量
-url="https://www.google.com.hk"
+url="https://www.youtube.com"
 time=80s
 
 # 启动进程
@@ -10,15 +10,14 @@ function satrt_pid()
     socks5_decide
     pm2_status
     if [[ $agent == "yes" ]]; then
-        echo $proxy
-        code=`curl --socks5 k1483162508.fun:9000 -I -m 30 -o /dev/null -s -w %{http_code}"\n" $url`
+        code=`curl --socks5 $proxy -I -m 30 -o /dev/null -s -w %{http_code}"\n" $url`
         echo $code
         code_status
     elif [[ $agent == "no" ]] && [[ $mtp_addr == "no" ]]; then
         code=`curl -I -m 30 -o /dev/null -s -w %{http_code}"\n" $url`
         code_status
     else
-        echo "...."
+        echo "..."
     fi
 }
 
@@ -52,7 +51,7 @@ function socks5_decide {
     j=1
     agent="no"
     mtp_addr="no"
-    desStr5=`cat config.yml | grep $desStr5`
+    desStr5=`cat /pagermaid/workdir/config.yml | grep $desStr5`
     desStr5=`echo $desStr5 | sed -r 's/.*"(.+)".*/\1/'`
     if [[ $desStr5 == 'mtp_addr: ""' ]]; then
         while [[ j -le 3 ]]; do
